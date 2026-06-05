6f90354368d841af66601b6be0ab19cd5a5d32b7
jenniferkropke-cmd <jenniferkropke@gmail.com>
2026-06-05 15:13:48 -0500

    Update generate-blog-index.mjs

--- scripts/generate-blog-index.mjs
+++ scripts/generate-blog-index.mjs
@@ -33,12 +33,10 @@ const files = fs.readdirSync(blogDir).filter(file => file.endsWith('.md'));
 if (files.length === 0) {
   fs.writeFileSync(outputFile, JSON.stringify([], null, 2));
 
-  fs.writeFileSync(
-    sitemapFile,
-    `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-</urlset>`
-  );
+fs.writeFileSync(
+  sitemapFile,
+  '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n</urlset>'
+);
 
   process.exit(0);
 }
